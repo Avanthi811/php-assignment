a244d6888bdc17684f05079c32364c461470b8fd
Avanthi811 <avanthikanamarlapudi@gmail.com>
2025-05-06 23:30:20 +0530

    assignment

--- question.7.php
+++ question.7.php
@@ -3,10 +3,10 @@ $num_1 = (int) readline();
 $num_2 = (int) readline();
 $quotient = $num_1/ $num_2;
 $remaider = $num_1 % $num_2;
-if ($num_2 != 0){
+try{
     echo "Quotient is : " . $quotient . "\n";
     echo "Remainder is :" .$remaider ."\n";
-}else {
+}catch (Exception $e){
     echo "Zero division error\n";
 }
 ?>
